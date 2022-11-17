52ec0e6cf0f9b8c3595d98bb48b5b446765fd0b9
Geon_27 <yukgeon98527@gmail.com>
2022-11-17 19:04:57 +0900

    1117, final

--- SubmitValue/ViewController.swift
+++ SubmitValue/ViewController.swift
@@ -4,31 +4,9 @@
 //
 //  Created by 육건 on 2022/11/15.
 //
-
 import UIKit
 class ViewController: UIViewController {
     
-    @IBAction func onSubmit(_sender: Any) {
-        // VC2의 인스턴스 생성
-        guard let rvc = self.storyboard?.instantiateViewController(withIdentifier: "RVC") as? ResultViewController else {
-            return
-        }
-    }
-    // 자동 갱신 여부가 바뀔 때마다 호출되는 메소드
-    @IBAction func onSwitch(_ sender: UISwitch) {
-        if(sender.isOn == true) {
-            self.isUpdateText.text = "갱신함"
-        } else {
-            self.isUpdateText.text = "갱신하지 않음"
-        }
-    }
-
-    // 갱신 주기가 바뀔때 마다 호출되는 메소드
-    @IBAction func onStepper(_ sender: UIStepper) {
-        let value = Int(sender.value)
-        self.intervalText.text = "\(value) 분마다"
-    }
-
     // 이메일 주소를 입력받는 Textfield
     @IBOutlet var email: UITextField!
     
@@ -43,4 +21,34 @@ class ViewController: UIViewController {
     
     // 갱신 주기를 분단위 + 텍스트로 표시하는 Lable
     @IBOutlet var intervalText: UILabel!
+    
+    // 자동 갱신 여부가 바뀔 때마다 호출되는 메소드
+    @IBAction func onSwitch(_ sender: UISwitch) {
+        if(sender.isOn == true) {
+            self.isUpdateText.text = "갱신함"
+        } else {
+            self.isUpdateText.text = "갱신하지 않음"
+        }
+    }
+
+    // 갱신 주기가 바뀔때 마다 호출되는 메소드
+    @IBAction func onStepper(_ sender: UIStepper) {
+        let value = Int(sender.value)
+        self.intervalText.text = "\(value) 분마다"
+    }
+    
+    @IBAction func onSubmit(_sender: Any) {
+        // VC2의 인스턴스 생성
+        guard let rvc = self.storyboard?.instantiateViewController(withIdentifier: "RVC") as? ResultViewController else {
+            return
+        }
+        
+        // 값 전달
+        rvc.paramEmail = self.email.text! //이메일
+        rvc.paramUpdate = self.isUpdate.isOn // 자동갱신 여부
+        rvc.paramInterval = self.interval.value //갱신주기
+        
+        // 화면 이동
+        self.present(rvc, animated: true)
+    }
 }
